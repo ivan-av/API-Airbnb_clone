4c0bccc57d1ea18d5dcb2782db53785bc0030200
Iván Avila <ivan.avicamp@gmail.com>
2022-09-14 23:58:13 -0500

    database connection setting

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,13 +1,23 @@
 const { Sequelize } = require('sequelize');
 
 // Create a connection to database
-const db = new Sequelize({
-  dialect: 'postgres',
-  host: 'localhost',
-  username: 'postgres',
-  password: 'd3s5rr0ll0',
-  database: 'airbnb',
 
+const db = new Sequelize({ 
+  dialect: 'postgres', 
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB,
+  logging: false,
+  dialectOptions:
+    process.env.NODE_ENV === 'production'
+      ? {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
+        }
+      : {},
 });
 
 module.exports = { db };
